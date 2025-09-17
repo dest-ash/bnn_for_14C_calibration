8eb96fce1bc449c34adf8b437168ddc9f6c328b7
destash <destin.cirumanga@gmail.com>
2025-09-17 12:25:11 +0200

    finalisation du module de gestion du cache local

--- src/bnn_for_14C_calibration/manage_cache.py
+++ src/bnn_for_14C_calibration/manage_cache.py
@@ -1,15 +1,81 @@
 # -*- coding: utf-8 -*-
 
+
 from pathlib import Path
+import requests
+import shutil
 
 
-# URL pour accéder aux données distantes à mettre en cache
-CACHE_URL = "https://raw.githubusercontent.com/dest-ash/bnn_for_14C_calibration/main/models/"
+# ========================================================================
+# définition des chemins constants vers le cache local 
+# ========================================================================
+
+# chemin de sauvegarde pour le cache local
+CACHE_DIR_NAME = ".bnn_for_14C_calibration"
+CACHE_DIR = Path.home().resolve() / CACHE_DIR_NAME
+
+# API GitHub pour lister le contenu le dossier 'models' destiné au cache local
+# et chemin pour le dossier  distant 'models' dans le cache local
+MODELS_DIR_API_URL = "https://api.github.com/repos/dest-ash/bnn_for_14C_calibration/contents/models"
+MODELS_DIR_LOCAL = CACHE_DIR / "models"
+
+# ========================================================================
+# fonctions de téléchargement des données et leur mise en cache
+# ========================================================================
+
+
+# téléchargement d'un dossier github
+def download_github_folder(
+    api_url: str, 
+    local_dir: Path
+):
+    response = requests.get(api_url)
+    response.raise_for_status()
+    items = response.json()
+
+    for item in items:
+        if item["type"] == "file":
+            file_url = item["download_url"]
+            local_path = local_dir / item["name"]
+            print(f"Downloading {file_url} → {local_path}")
+            r = requests.get(file_url)
+            r.raise_for_status()
+            local_path.write_bytes(r.content)
+        elif item["type"] == "dir":
+            subdir = local_dir / item["name"]
+            subdir.mkdir(exist_ok=True)
+            download_github_folder(item["url"], subdir)
+
+
+def clear_cache():
+    """
+    Supprime complètement le dossier cache de la librairie.
+    """
+    if CACHE_DIR.exists():
+        print(f"🗑️ removing cache directory at : {CACHE_DIR}")
+        shutil.rmtree(CACHE_DIR)
+        print(f"🗑️ cache removed!")
+    else:
+        print("ℹ️ No existing cache!")
 
 def download_cache_lib_data(
     overwrite = False
 ):
-    url = "https://github.com/dest-ash/bnn_for_14C_calibration/tree/main/models"
-    cache_dir_name = ".bnn_for_14C_calibration"
-    path_to_user_home = Path.home().resolve()
+    if overwrite or not (CACHE_DIR.exists() and CACHE_DIR.is_dir())
+        print(f"Creating cache directory at: {CACHE_DIR}")
+        CACHE_DIR.mkdir(parents=True, exist_ok=True)
+        MODELS_DIR_LOCAL.mkdir(exist_ok=True)
+        download_github_folder(MODELS_DIR_API_URL, MODELS_DIR_LOCAL)
+        print(f"""
+        ✅ Cache directory created at: {CACHE_DIR}, and filled with all 
+        the contents of the 'models' directory downloaded from GitHub.
+        """)
+    else :
+        print(f"""
+            An existing cache directory is located at {CACHE_DIR}  and overwrite is {overwrite}.
+            If you wish to force the cache download, set overwrite to True.
+        """)
+
+if __name__ == "__main__":
+    download_cache_lib_data()
 
